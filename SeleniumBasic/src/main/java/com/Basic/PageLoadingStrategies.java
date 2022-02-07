48aa4c443d97936df4dd078d2d6f53a2c889e122
fahim59 <mrahaman59@yahoo.com>
2022-02-07 18:20:20 +0600

    Page Loading Strategies

--- SeleniumBasic/src/main/java/com/Basic/PageLoadingStrategies.java
+++ SeleniumBasic/src/main/java/com/Basic/PageLoadingStrategies.java
@@ -7,16 +7,31 @@ import org.openqa.selenium.firefox.*;
 public class PageLoadingStrategies extends BaseClass {
     public static void main(String[] args) throws InterruptedException {
         FirefoxLaunch();
+
+        //Normal();
+        //Eager();
+        //None();
+
         OpenWebsite("https://www.daraz.com.bd/");
-
-        Normal();
-
         SmallWait();
         FirefoxQuit();
     }
     public static void Normal(){
         //Waits for the entire page to be loaded
+
         FirefoxOptions options = new FirefoxOptions();
         options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+    }
+
+    public static void Eager(){
+        //Waits until the initial HTML document has been completely loaded and parsed
+        FirefoxOptions options = new FirefoxOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+    }
+
+    public static void None(){
+        //Waits only the initial page is downloaded
+        FirefoxOptions options = new FirefoxOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
     }
 }
