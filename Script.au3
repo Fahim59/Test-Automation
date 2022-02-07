b89046669ef40e86ba7d86192c26d51c0e8d2697
fahim59 <mrahaman59@yahoo.com>
2022-02-08 01:03:30 +0600

    All Actions

---
+++ Script.au3
@@ -0,0 +1,3 @@
+ControlFocus("File Upload","","Edit1")
+ControlSetText("File Upload","","Edit1","F:\JAVA\ALL CODES\Code\IntelliJ\Link.txt")
+ControlClick("File Upload","","Button1")
--- SeleniumBasic/src/main/java/com/Basic/AllActions.java
+++ SeleniumBasic/src/main/java/com/Basic/AllActions.java
@@ -5,17 +5,25 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.*;
 import org.openqa.selenium.support.ui.*;
 
+import java.io.*;
 import java.util.*;
 
 public class AllActions extends BaseClass {
-    public static void main(String[] args) throws InterruptedException {
+    public static String encode = "QXBwaXVtMTIzQA==";
+
+    public static void main(String[] args) throws InterruptedException, IOException {
         FirefoxLaunch();
 
         //KeyboardActions();
         //MouseActions();
+        //DragandDrop();
         //DropDown();
         //RadioButton();
-        IFrame();
+        //IFrame();
+        //Slider();
+        //EncodeDecode();
+        //FileUpload();
+        DateTime();
 
         SmallWait();
         FirefoxQuit();
@@ -60,7 +68,6 @@ public class AllActions extends BaseClass {
         //driver.navigate().to("https://demoqa.com/buttons");
 
         //DoubleClick
-
         /*WebElement doubleClickBtn = driver.findElement(By.id("doubleClickBtn"));
         actions.doubleClick(doubleClickBtn).perform();
 
@@ -84,13 +91,38 @@ public class AllActions extends BaseClass {
         }*/
         //-------------------------------------------------------------------------------//
 
-        //DragAndDrop
-        driver.navigate().to("https://selenium08.blogspot.com/2020/01/drag-drop.html");
+        //Resize
+        driver.navigate().to("https://jqueryui.com/resizable/");
+
+        driver.switchTo().frame(0);
+
+        WebElement resize = driver.findElement(By.xpath("/html/body/div/div[3]"));
+        actions.dragAndDropBy(resize, 100, 50).perform();
+
+        Thread.sleep(3000);
+    }
+
+    public static void DragandDrop(){
+        Actions action = new Actions(driver);
+
+        //Drag
+        driver.navigate().to("https://jqueryui.com/draggable/");
+
+        List<WebElement> frame = driver.findElements(By.tagName("iframe"));
+
+        driver.switchTo().frame(0);
+        WebElement drag = driver.findElement(By.xpath("//*[@id=\"draggable\"]"));
+        System.out.println(drag.getText());
+
+        action.moveToElement(drag).dragAndDropBy(drag, 50,0).build().perform();
+        //--------------------------------------------------------------------------------//
+        //Drag and Drop
+        /*driver.navigate().to("https://selenium08.blogspot.com/2020/01/drag-drop.html");
 
         WebElement drag  = driver.findElement(By.xpath("//*[@id=\"draggable\"]"));
         WebElement drop  = driver.findElement(By.xpath("//*[@id=\"droppable\"]"));
 
-        actions.dragAndDrop(drag,drop).build().perform();
+        actions.dragAndDrop(drag,drop).build().perform();*/
     }
 
     public static void DropDown() throws InterruptedException {
@@ -149,6 +181,7 @@ public class AllActions extends BaseClass {
         //System.out.println(frame.size());
 
         //1st method, find frame by index
+
         driver.switchTo().frame(0);
         WebElement element = driver.findElement(By.xpath("//*[@id=\"draggable\"]"));
         System.out.println(element.getText());
@@ -159,6 +192,7 @@ public class AllActions extends BaseClass {
         //----------------------------------------------------------------------------------------------//
 
         /*//2nd Method, find frame by xpath
+
         WebElement frame1 = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/iframe"));
         driver.switchTo().frame(frame1);
         WebElement element2 = driver.findElement(By.xpath("//*[@id=\"draggable\"]"));
@@ -167,5 +201,74 @@ public class AllActions extends BaseClass {
         driver.switchTo().defaultContent(); //to find other elements outside the frame switch to is must
         WebElement element3 = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/p[1]"));
         System.out.println(element1.getText());*/
+    }
+
+    public static void Slider() throws InterruptedException {
+        Actions action = new Actions(driver);
+
+        driver.navigate().to("https://jqueryui.com/slider/");
+
+        List<WebElement> frame = driver.findElements(By.tagName("iframe"));
+
+        driver.switchTo().frame(0);
+        WebElement slider = driver.findElement(By.xpath("//*[@id=\"slider\"]"));
+
+        action.moveToElement(slider).dragAndDropBy(slider,30,0).build().perform();
+
+        Thread.sleep(3000);
+    }
+
+    public static String Password(){ //Use this on another class
+        /*String password = "Appium123@"; //QXBwaXVtMTIzQA==
+
+        String encode = new String (Base64.getEncoder().encode(password.getBytes()));
+        String decode = new String (Base64.getDecoder().decode(encode.getBytes()));
+
+        System.out.println(encode);
+        System.out.println(decode);*/
+
+        return new String (Base64.getDecoder().decode(encode.getBytes()));
+    }
+
+    public static void EncodeDecode(){
+        driver.navigate().to("https://www.rediff.com/");
+
+        driver.findElement(By.linkText("Sign in")).click();
+        driver.findElement(By.id("login1")).sendKeys("seleniumt@rediffmail.com");
+        driver.findElement(By.id("password")).sendKeys(Password());
+        driver.findElement(By.name("proceed")).click();
+    }
+
+    public static void FileUpload() throws IOException, InterruptedException {
+        driver.navigate().to("https://www.file.io/");
+
+        driver.findElement(By.xpath("/html/body/div/div/div[1]/div[2]/header/div/div[2]/div[1]/div[2]/label")).click();
+
+        Runtime.getRuntime().exec("F:\\JAVA\\ALL CODES\\Code\\IntelliJ\\Script.exe");
+
+        Thread.sleep(5000);
+    }
+
+    public static void DateTime() throws InterruptedException {
+        String month = "March 2022";
+
+        driver.navigate().to("https://www.shohoz.com/bus-tickets");
+
+        driver.findElement(By.xpath("//*[@id=\"doj\"]")).click();
+
+        while(true){
+            String text = driver.findElement(By.className("ui-datepicker-title")).getText();
+            //System.out.println(text);
+            if(text.equals(month)){
+                break;
+            }
+            else {
+                driver.findElement(By.cssSelector(".ui-icon-circle-triangle-e")).click();
+            }
+        }
+
+        driver.findElement(By.xpath("//a[normalize-space()='20']")).click();
+
+        Thread.sleep(5000);
     }
 }
