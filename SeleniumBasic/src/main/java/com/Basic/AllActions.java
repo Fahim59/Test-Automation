f2f08e350e5773738bb5e2e7c76b24a1150bfad9
fahim59 <mrahaman59@yahoo.com>
2022-02-13 14:46:13 +0600

    File Download

--- SeleniumBasic/src/main/java/com/Basic/AllActions.java
+++ SeleniumBasic/src/main/java/com/Basic/AllActions.java
@@ -2,10 +2,13 @@ package com.Basic;
 
 import com.Base.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.interactions.*;
 import org.openqa.selenium.support.ui.*;
 
 import java.io.*;
+import java.net.*;
+import java.time.*;
 import java.util.*;
 
 public class AllActions extends BaseClass {
@@ -17,14 +20,16 @@ public class AllActions extends BaseClass {
         //KeyboardActions();
         //MouseActions();
         //DragandDrop();
-        DropDown();
+        //DropDown();
         //RadioButton();
         //Checkbox();
         //IFrame();
         //Slider();
         //EncodeDecode();
         //FileUpload();
+        FileDownload();
         //DateTime();
+        //BrokenLink();
 
         SmallWait();
         FirefoxQuit();
@@ -405,5 +410,42 @@ public class AllActions extends BaseClass {
         driver.findElement(By.xpath("//a[normalize-space()='1']")).click();*/
         //---------------------------------------------------------------------------------//
         driver.findElement(By.id("vfb-18")).sendKeys("03/01/2023" +Keys.ENTER);
+    }
+
+    public static void BrokenLink() throws InterruptedException, IOException {
+        /*A broken link is a link on a web page that no longer works because of one the following reasons
+        --> the url is improper
+        --> the destination website removed the linked webpage(404 error)
+        --> the destination website is permanently removed or no longer exists
+         */
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.navigate().to("https://demo.guru99.com/V4/index.php");
+
+        Thread.sleep(5000);
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
+        for(int i = 0; i < links.size(); i++){
+            WebElement element = links.get(i);
+            String url = element.getAttribute("href");
+
+            URL link = new URL(url);
+
+            HttpURLConnection httpCom = (HttpURLConnection) link.openConnection();
+            Thread.sleep(3000);
+
+            httpCom.connect();
+
+            int code = httpCom.getResponseCode();
+
+            if(code >= 400){
+                System.out.println(url +" - "+ " is broken");
+            }
+            else {
+                System.out.println(url +" - "+ " is not broken");
+            }
+        }
     }
 }
